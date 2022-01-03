e998e231d88026df0c91c548fc255f654678a463
mikel-js <michaelcastro.js@gmail.com>
2022-01-03 07:54:38 +0200

    feat: added stlye variables

--- src/pages/Experience/index.js
+++ src/pages/Experience/index.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import ThreeScene from './Components/ThreeScene';
 const Experience = () => {
-  const [darkMode, setDarkMode] = useState(true);
+  const [darkMode, setDarkMode] = useState(false);
 
   const toggleDarkMode = () => setDarkMode(!darkMode);
   return (
     <div className='experience'>
       <div className='experience__left'>
-        <button onClick={toggleDarkMode}>Activate Light</button>
+        <div className='experience__left-button'>
+          <button onClick={toggleDarkMode}>Toggle Background</button>
+        </div>
       </div>
       <ThreeScene darkMode={darkMode} />
     </div>
